58733268cada0b26368139961447911b5e1e0381
bipul shrestha <bipulshr28@gmail.com>
2024-12-13 13:47:11 +0545

    simple request test for getting the team

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,19 +1,41 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import Layout from "../components/Layout";
 import "../css/Team.css";
 import TeamModal from "../components/TeamModal";
+import axios from "axios";
+
+const API_BASE_URL = "http://localhost:8080/api/teams";
 
 const Team = () => {
-    const [teamData, setTeamData] = useState([
-        { id: 1, name: "Team Kathmandu" },
-        { id: 2, name: "Team Chitwan" },
-        { id: 3, name: "Team Biratnagar" },
-    ]);
+    const [teamData, setTeamData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null)
 
     const [isModalOpen, setModalOpen] = useState(false);
+
+    const fetchItem = async () => {
+        try{
+            setLoading(true);
+            const response = await axios.get(API_BASE_URL)
+            const cleanedData = response.data.map(item =>{
+                const {players, ...rest} = item
+                return rest;
+            })
+            console.log(cleanedData)
+            setTeamData(cleanedData)
+        }catch (e) {
+            console.log("error occured")
+        }finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchItem()
+    }, []);
 
     const handleDelete = (id) => {
         setTeamData(teamData.filter((row) => row.id !== id));
@@ -60,6 +82,7 @@ const Team = () => {
                 </button>
                 <div className="ag-theme-alpine" style={{ height: 400, width: "100%" }}>
                     <AgGridReact
+                        key={teamData.length}
                         rowData={teamData}
                         columnDefs={columnDefs}
                         domLayout="autoHeight"
